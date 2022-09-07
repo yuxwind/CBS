d085497f8cfc578046599ab0109aed9473ac770d
yuxwind <yuxwind@gmail.com>
2022-09-07 16:43:56 -0600

    update script

--- scripts/sweep_mnist_mlpnet_woodfisher_no_weight_update.sh
+++ scripts/sweep_mnist_mlpnet_woodfisher_no_weight_update.sh
@@ -77,7 +77,7 @@ ${ONE_SHOT} \
 --not-update-weights \
 "
 if [ "$is_test" -eq 0 ] ; then
-    CUDA_VISIBLE_DEVICES=${GPU} python ${CODE_DIR}/main.py $args $greedy_args &> $LOG_PATH 2>&1
+    CUDA_VISIBLE_DEVICES=${GPU} python ${CODE_DIR}/main.py $args $greedy_args > $LOG_PATH 2>&1
 else
     CUDA_VISIBLE_DEVICES=${GPU} python ${CODE_DIR}/main.py $args $greedy_args
 fi
